1c2422020d5d8f3d7bd85909bc56d369d5d6b163
Han <philong07712@gmail.com>
2021-04-03 10:56:42 +0700

    + Init serverapp

---
+++ src/main/java/com/example/DAVDK/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.DAVDK;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
